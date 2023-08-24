bbaab66666449726931fdfa76733b36df60d2bdb
TechnoGirl <93014197+technogirl2@users.noreply.github.com>
2023-08-24 14:21:14 -0500

    Add files via upload

---
+++ static/javascript/about.js
@@ -0,0 +1,10 @@
+
+var button = document.getElementById('musicBt')
+
+var isPlay = False;
+var back = document.getElementById('d4');
+
+button.addEventListener('click', function(){
+    d4.css('background', "snow")
+     
+});
